06ffdb22d0b0ef955915368d6d730efc2388ca16
Michalis Meliopoulos <mixalismeliop@gmail.com>
2023-05-24 16:20:01 +0300

    metadata fix

--- app/[searchTerm]/loading.tsx
+++ app/[searchTerm]/loading.tsx
@@ -1,7 +1,7 @@
 export default function loading() {
   return (
-    <main className="bg-slate-200 mx-auto max-w-lg p-1 min-h-screen">
-      <h2 className="m-4 text-2xl font-bold">Loading...</h2>
+    <main className="bg-slate-200 mx-auto max-w-2xl p-2 min-h-screen flex align-center justify-center">
+      <h2 className="text-2xl font-bold">Loading...</h2>
     </main>
   );
 }
